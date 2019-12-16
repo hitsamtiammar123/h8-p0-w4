3c4e9869a4fbb4a0e88307a38f1db22cbfda0b71
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2019-12-16 23:35:39 +0700

    add exercise-6 not finished

---
+++ exercise-6.js
@@ -0,0 +1,16 @@
+function digitPerkalianMinimum(angka) {
+    // you can only write your code here!
+    var minDig=0;
+    var dump=[];
+    var c1=1;
+    var c2=1;
+
+    return minDig;
+  }
+  
+  // TEST CASES
+  console.log(digitPerkalianMinimum(24)); // 2
+  console.log(digitPerkalianMinimum(90)); // 3
+  console.log(digitPerkalianMinimum(20)); // 2
+  console.log(digitPerkalianMinimum(179)); // 4
+  console.log(digitPerkalianMinimum(1)); // 2
