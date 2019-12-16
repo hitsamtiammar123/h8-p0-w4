44b026c33d097f38c8db890d198f68de790d15ac
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2019-12-17 00:10:40 +0700

    modifikasi sedikit untuk exercise-6.js

--- exercise-6.js
+++ exercise-6.js
@@ -4,6 +4,23 @@ function digitPerkalianMinimum(angka) {
     var dump=[];
     var c1=1;
     var c2=1;
+    var list=[];
+
+    if(angka===1)
+        return 2;
+    else{
+        while(dump.indexOf(c1)===-1){
+            if(angka % c1===0){
+                c2=angka / c1;
+                dump.push(c2);
+                list.push([c1,c2]);
+                console.log(list);
+                if(c2.toString().length===1 && c1.toString().length===1 )
+                    minDig++; 
+            }
+            c1++;
+        }
+    }
 
     return minDig;
   }
